7398ff8d13a71d919ef0227143dca3b7d078b0ba
Santeri Saariokari <santeri.saariokari@cern.ch>
2022-05-13 16:31:28 +0200

    flags

--- vdm.py
+++ vdm.py
@@ -1,4 +1,5 @@
-import os
+from pathlib import Path
+import argparse
 
 import pandas as pd
 import numpy as np
@@ -11,90 +12,96 @@ from iminuit.cost import LeastSquares
 def gaussian(x, peak, mean, cap_sigma):
     return peak*np.exp(-(x-mean)**2/(2*cap_sigma**2))
 
-os.makedirs('output', exist_ok=True)
+def main(args):
+    for filename in args.files:
+        outpath = f'output/{Path(filename).stem}'
+        Path(outpath).mkdir(parents=True, exist_ok=True)
+        with tables.open_file(filename, 'r') as f:
+            collidable = np.nonzero(f.root.beam[0]['collidable'])[0] # indices of colliding
 
-data = pd.DataFrame()
-with tables.open_file('7525_2110302352_2110310014.hd5', 'r') as f:
-    collidable = np.nonzero(f.root.beam[0]['collidable'])[0] # indices of colliding
+            scan = pd.DataFrame()
+            scan['timestampsec'] = [r['timestampsec'] for r in f.root.vdmscan.where('stat == "ACQUIRING"')]
+            scan['sep'] = [r['sep'] for r in f.root.vdmscan.where('stat == "ACQUIRING"')]
+            scan['nominal_sep_plane'] = [r['nominal_sep_plane'].decode('utf-8') for r in f.root.vdmscan.where('stat == "ACQUIRING"')]
+            scan = scan.groupby(['nominal_sep_plane', 'sep']).agg(min_time=('timestampsec', np.min), max_time=('timestampsec', np.max))
 
-    scan = pd.DataFrame()
-    scan['timestampsec'] = [r['timestampsec'] for r in f.root.vdmscan.where('stat == "ACQUIRING"')]
-    scan['sep'] = [r['sep'] for r in f.root.vdmscan.where('stat == "ACQUIRING"')]
-    scan['nominal_sep_plane'] = [r['nominal_sep_plane'].decode('utf-8') for r in f.root.vdmscan.where('stat == "ACQUIRING"')]
-    scan = scan.groupby(['nominal_sep_plane', 'sep']).agg(min_time=('timestampsec', np.min), max_time=('timestampsec', np.max))
+            scan.reset_index(inplace=True)
 
-    scan.reset_index(inplace=True)
+            rates = pd.DataFrame()
 
-    rates = pd.DataFrame()
+            for p, plane in enumerate(scan.nominal_sep_plane.unique()):
+                for b, sep in enumerate(scan.sep.unique()):
+                    period_of_scanpoint = f'(timestampsec > {scan.min_time[(scan.nominal_sep_plane == plane) & (scan.sep == sep)].item()}) & (timestampsec <= {scan.max_time[(scan.nominal_sep_plane == plane) & (scan.sep == sep)].item()})'
+                    
+                    r = np.array([r['bxraw'][collidable] for r in f.root[args.luminometer].where(period_of_scanpoint)])
+                    beam = np.array([b['bxintensity1'][collidable]*b['bxintensity2'][collidable]/1e22 for b in f.root['beam'].where(period_of_scanpoint)])
+                    new_rates = pd.DataFrame(np.array([r.mean(axis=0), beam.mean(axis=0)]).T, columns=['rate', 'beam'])
 
-    for p, plane in enumerate(scan.nominal_sep_plane.unique()):
-        for b, sep in enumerate(scan.sep.unique()):
-            period_of_scanpoint = f"(timestampsec > {scan.min_time[(scan.nominal_sep_plane == plane) & (scan.sep == sep)].item()}) & (timestampsec <= {scan.max_time[(scan.nominal_sep_plane == plane) & (scan.sep == sep)].item()})"
-            
-            r = np.array([r['bxraw'][collidable] for r in f.root['pltlumizero'].where(period_of_scanpoint)])
-            beam = np.array([b['bxintensity1'][collidable]*b['bxintensity2'][collidable]/1e22 for b in f.root['beam'].where(period_of_scanpoint)])
-            new_rates = pd.DataFrame(np.array([r.mean(axis=0), beam.mean(axis=0)]).T, columns=['rate', 'beam'])
+                    new_rates.insert(0, 'bcid', collidable+1)
+                    new_rates.insert(0, 'sep', sep)
+                    new_rates.insert(0, 'plane', plane)
 
-            new_rates.insert(0, 'bcid', collidable+1)
-            new_rates.insert(0, 'sep', sep)
-            new_rates.insert(0, 'plane', plane)
+                    new_rates['rate_normalised'] = new_rates['rate'] / new_rates['beam']
+                    new_rates['rate_normalised_err'] = stats.sem(r, axis=0) / new_rates['beam']
 
-            new_rates['rate_normalised'] = new_rates['rate'] / new_rates['beam']
-            new_rates['rate_normalised_err'] = stats.sem(r, axis=0) / new_rates['beam']
+                    rates = new_rates if p == 0 and b == 0 else pd.concat([rates, new_rates])
 
-            rates = new_rates if p == 0 and b == 0 else pd.concat([rates, new_rates])
+        rates.to_csv(f'{outpath}/data.csv', index=False)
 
-rates.to_csv('output/data.csv', index=False)
+        rates['rate_normalised_err'].replace(0, rates['rate_normalised_err'].max(axis=0), inplace=True) # Add sensible error in case of 0 rate
 
-rates['rate_normalised_err'].replace(0, rates['rate_normalised_err'].max(axis=0), inplace=True) # Add sensible error in case of 0 rate
+        for p, plane in enumerate(rates.plane.unique()):
+            for b, bcid in enumerate(rates.bcid.unique()):
+                plt.figure()
+                plt.title(f'{plane}, BCID {bcid+1}')
+                data_x = scan[scan.nominal_sep_plane == plane]['sep']
+                data_y = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised
+                data_y_err = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised_err
 
-for p, plane in enumerate(rates.plane.unique()):
-    for b, bcid in enumerate(rates.bcid.unique()):
-        plt.figure()
-        plt.title(f"{plane}, BCID {bcid+1}")
-        data_x = scan[scan.nominal_sep_plane == plane]["sep"]
-        data_y = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised
-        data_y_err = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised_err
+                least_squares = LeastSquares(data_x, data_y, data_y_err, gaussian)
+                m = Minuit(least_squares, peak=1e-4, mean=0, cap_sigma=0.3)
+                m.migrad()  # finds minimum of least_squares function
+                m.hesse()   # accurately computes uncertainties
 
-        least_squares = LeastSquares(data_x, data_y, data_y_err, gaussian)
-        m = Minuit(least_squares, peak=1e-4, mean=0, cap_sigma=0.3)
-        m.migrad()  # finds minimum of least_squares function
-        m.hesse()   # accurately computes uncertainties
+                new = pd.DataFrame([m.values, m.errors], columns=m.parameters)
+                new.insert(0, 'what', ['value', 'error'])
+                new.insert(0, 'plane', plane)
+                new.insert(0, 'bcid', bcid)
 
-        new = pd.DataFrame([m.values, m.errors], columns=m.parameters)
-        new.insert(0, 'what', ['value', 'error'])
-        new.insert(0, 'plane', plane)
-        new.insert(0, 'bcid', bcid)
+                fit_results = new if b == 0 and p == 0 else pd.concat([fit_results, new], ignore_index=True)
 
-        fit_results = new if b == 0 and p == 0 else pd.concat([fit_results, new], ignore_index=True)
+                plt.errorbar(data_x, data_y, data_y_err, fmt='o')
+                x_dense = np.linspace(np.min(data_x), np.max(data_x))
+                plt.plot(x_dense, gaussian(x_dense, *m.values))
 
-        plt.errorbar(data_x, data_y, data_y_err, fmt="o")
-        x_dense = np.linspace(np.min(data_x), np.max(data_x))
-        plt.plot(x_dense, gaussian(x_dense, *m.values))
+                fit_info = [f'$\\chi^2$ / $n_\\mathrm{{dof}}$ = {m.fval:.1f} / {len(data_x) - m.nfit}']
 
-        fit_info = [f"$\\chi^2$ / $n_\\mathrm{{dof}}$ = {m.fval:.1f} / {len(data_x) - m.nfit}"]
+                for param, v, e in zip(m.parameters, m.values, m.errors):
+                    fit_info.append(f'{param} = ${v:.3e} \\pm {e:.3e}$')
 
-        for param, v, e in zip(m.parameters, m.values, m.errors):
-            fit_info.append(f"{param} = ${v:.3e} \\pm {e:.3e}$")
+                plt.legend(title='\n'.join(fit_info))
 
-        plt.legend(title="\n".join(fit_info))
+                plt.savefig(f'{outpath}/fit_{plane}_{bcid}.png')
 
-        plt.savefig(f'output/fit_{plane}_{bcid}.png')
+        fit_results.cap_sigma *= 1e3 # to µm
 
-fit_results.cap_sigma *= 1e3
+        values = fit_results[fit_results.what == 'value']
+        errors = fit_results[fit_results.what == 'error']
 
-values = fit_results[fit_results.what == 'value']
-errors = fit_results[fit_results.what == 'error']
+        val = values.pivot(index='bcid', columns=['plane'], values=['cap_sigma', 'peak'])
+        err = errors.pivot(index='bcid', columns=['plane'], values=['cap_sigma', 'peak'])
 
-val = values.pivot(index='bcid', columns=['plane'], values=['cap_sigma', 'peak'])
-err = errors.pivot(index='bcid', columns=['plane'], values=['cap_sigma', 'peak'])
+        sigvis = np.pi * val.cap_sigma.prod(axis=1) * val.peak.sum(axis=1)
 
-sigvis = np.pi * val.cap_sigma.prod(axis=1) * val.peak.sum(axis=1)
+        sigvis_err = (err.cap_sigma**2 / val.cap_sigma**2).sum(axis=1) + (err.peak**2).sum(axis=1) / (val.peak).sum(axis=1)**2
+        sigvis_err = np.sqrt(sigvis_err) * sigvis
 
-sigvis_err = (err.cap_sigma**2 / val.cap_sigma**2).sum(axis=1) + (err.peak**2).sum(axis=1) / (val.peak).sum(axis=1)**2
-sigvis_err = np.sqrt(sigvis_err) * sigvis
+        lumi = pd.concat([sigvis, sigvis_err], axis=1)
+        lumi.columns = ['sigvis', 'sigvis_err']
+        lumi.to_csv(f'{outpath}/lumi.csv')
 
-lumi = pd.concat([sigvis, sigvis_err], axis=1)
-lumi.columns = ['sigvis', 'sigvis_err']
-lumi.to_csv('output/lumi.csv')
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--luminometer', type=str, help='luminometer name', required=True)
+    parser.add_argument('files', nargs='*')
+    main(parser.parse_args())
