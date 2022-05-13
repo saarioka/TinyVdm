7bd8bc333b1c7b1ceb1e2be806c61533061e2ccf
Santeri Saariokari <santeri.saariokari@cern.ch>
2022-05-13 16:58:56 +0200

    plots to pdf

--- vdm.py
+++ vdm.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import tables
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from scipy import stats
 from iminuit import Minuit
 from iminuit.cost import LeastSquares
@@ -46,42 +47,43 @@ def main(args):
 
                     rates = new_rates if p == 0 and b == 0 else pd.concat([rates, new_rates])
 
-        rates.to_csv(f'{outpath}/data.csv', index=False)
+        rates.to_csv(f'{outpath}/input_data.csv', index=False)
 
         rates['rate_normalised_err'].replace(0, rates['rate_normalised_err'].max(axis=0), inplace=True) # Add sensible error in case of 0 rate
 
-        for p, plane in enumerate(rates.plane.unique()):
-            for b, bcid in enumerate(rates.bcid.unique()):
-                plt.figure()
-                plt.title(f'{plane}, BCID {bcid+1}')
-                data_x = scan[scan.nominal_sep_plane == plane]['sep']
-                data_y = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised
-                data_y_err = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised_err
+        with PdfPages(f'{outpath}/fit_{args.luminometer}.pdf') as pdf:
+            for p, plane in enumerate(rates.plane.unique()):
+                for b, bcid in enumerate(rates.bcid.unique()):
+                    plt.figure()
+                    plt.title(f'{plane}, BCID {bcid+1}')
+                    data_x = scan[scan.nominal_sep_plane == plane]['sep']
+                    data_y = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised
+                    data_y_err = rates[(rates.plane == plane) & (rates.bcid == bcid)].rate_normalised_err
 
-                least_squares = LeastSquares(data_x, data_y, data_y_err, gaussian)
-                m = Minuit(least_squares, peak=1e-4, mean=0, cap_sigma=0.3)
-                m.migrad()  # finds minimum of least_squares function
-                m.hesse()   # accurately computes uncertainties
+                    least_squares = LeastSquares(data_x, data_y, data_y_err, gaussian)
+                    m = Minuit(least_squares, peak=1e-4, mean=0, cap_sigma=0.3)
+                    m.migrad()  # finds minimum of least_squares function
+                    m.hesse()   # accurately computes uncertainties
 
-                new = pd.DataFrame([m.values, m.errors], columns=m.parameters)
-                new.insert(0, 'what', ['value', 'error'])
-                new.insert(0, 'plane', plane)
-                new.insert(0, 'bcid', bcid)
+                    new = pd.DataFrame([m.values, m.errors], columns=m.parameters)
+                    new.insert(0, 'what', ['value', 'error'])
+                    new.insert(0, 'plane', plane)
+                    new.insert(0, 'bcid', bcid)
 
-                fit_results = new if b == 0 and p == 0 else pd.concat([fit_results, new], ignore_index=True)
+                    fit_results = new if b == 0 and p == 0 else pd.concat([fit_results, new], ignore_index=True)
 
-                plt.errorbar(data_x, data_y, data_y_err, fmt='o')
-                x_dense = np.linspace(np.min(data_x), np.max(data_x))
-                plt.plot(x_dense, gaussian(x_dense, *m.values))
+                    plt.errorbar(data_x, data_y, data_y_err, fmt='ko')
+                    x_dense = np.linspace(np.min(data_x), np.max(data_x))
+                    plt.plot(x_dense, gaussian(x_dense, *m.values), 'k')
 
-                fit_info = [f'$\\chi^2$ / $n_\\mathrm{{dof}}$ = {m.fval:.1f} / {len(data_x) - m.nfit}']
+                    fit_info = [f'$\\chi^2$ / $n_\\mathrm{{dof}}$ = {m.fval:.1f} / {len(data_x) - m.nfit}']
 
-                for param, v, e in zip(m.parameters, m.values, m.errors):
-                    fit_info.append(f'{param} = ${v:.3e} \\pm {e:.3e}$')
+                    for param, v, e in zip(m.parameters, m.values, m.errors):
+                        fit_info.append(f'{param} = ${v:.3e} \\pm {e:.3e}$')
 
-                plt.legend(title='\n'.join(fit_info))
+                    plt.legend(title='\n'.join(fit_info))
 
-                plt.savefig(f'{outpath}/fit_{plane}_{bcid}.png')
+                    pdf.savefig()
 
         fit_results.cap_sigma *= 1e3 # to µm
 
